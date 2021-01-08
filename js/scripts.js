940456552959b92abff94201ce94a7336ab6f7d6
BwanaQ <thunjawax@gmail.com>
2021-01-08 18:43:17 +0300

    Add order pizza form

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+/*
+function Pizza(size, crust) {
+  this.size = size;
+  this.crust = crust;
+  this.toppings= [];
+}
+Pizza.prototype.total = function() {
+  return this.firstName + " " + this.lastName;
+}
+*/
