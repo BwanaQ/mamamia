57d5690d789eec6a5ca83726dcd5a2acd272036a
BwanaQ <thunjawax@gmail.com>
2021-01-09 09:15:17 +0300

    create crusts

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,22 @@ Pizza.prototype.total = function() {
   return this.firstName + " " + this.lastName;
 }
 */
+var order = [];
+//create all Pizza constructors
+function PizzaSize(pizzaName, pizzaPrice) {
+  this.pizzaName = pizzaName;
+  this.pizzaPrice = pizzaPrice;
+}
+
+function CrustType(crustName, crustPrice){
+  this.crustName = crustName;
+  this.crustPrice = crustPrice;
+}
+
+function Toppings(toppingName, toppingPrice){
+  this.toppingName=toppingName;
+  this.toppingPrice = toppingPrice
+}
+
+var small = new PizzaSize("Small", 550)
+
