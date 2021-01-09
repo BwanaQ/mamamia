a9304ad30e6e7aceb1b1bea226868e88f775f44b
BwanaQ <thunjawax@gmail.com>
2021-01-09 09:52:09 +0300

    initialize variables

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,7 @@ Pizza.prototype.total = function() {
   return this.firstName + " " + this.lastName;
 }
 */
+//create empty order
 var order = [];
 //create all Pizza constructors
 function PizzaSize(pizzaName, pizzaPrice) {
@@ -20,10 +21,29 @@ function CrustType(crustName, crustPrice){
   this.crustPrice = crustPrice;
 }
 
-function Toppings(toppingName, toppingPrice){
+function Topping(toppingName, toppingPrice){
   this.toppingName=toppingName;
-  this.toppingPrice = toppingPrice
+  this.toppingPrice = toppingPrice;
 }
+//create Pizzas
+var small = new PizzaSize("small", 550);
+var medium = new PizzaSize("medium", 830);
+var large = new PizzaSize("large",1050);
+var mega = new PizzaSize("mega", 1270);
 
-var small = new PizzaSize("Small", 550)
+// create Crusts
+var thinCrust = new PizzaSize("thin", 100);
+var mediumCrust = new PizzaSize("medium", 110);
+var thickCrust = new PizzaSize("thick",120);
+
+//create toppings
+var vegeterian = new Topping("vegan" , [70,100,140,200]);
+var pepperoni = new Topping("pepperoni", [100,150,200,250]);
+var hawaiian = new Topping("hawaiian" , [70,90,110,130]);
+var bbq = new Topping("bbq" , [170,200,230,270]);
+
+//create delivery boolean and delivery charge within nairobi
+var isDelivered = false;
+var deliveryCharge = 100;
+
 
